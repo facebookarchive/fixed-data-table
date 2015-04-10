816689809bd02516ee48e2c812a33791d81d26b5
Pieter Vanderwerff <me@pieter.io>
2015-04-10 00:36:54 -0700

    Fix some stuff that broke with new versions on packages.

--- build_helpers/buildStaticSite.sh
+++ build_helpers/buildStaticSite.sh
@@ -5,6 +5,6 @@ PATH=$(npm bin):$PATH
 rm -rf ./__site__
 rm -rf ./__site_prerender__
 ./build_helpers/buildAPIDocs.sh
-NODE_ENV=production webpack --config site/webpack-client.config.js
-NODE_ENV=production webpack --config site/webpack-prerender.config.js
+NODE_ENV=production webpack --config "$PWD/site/webpack-client.config.js"
+NODE_ENV=production webpack --config "$PWD/site/webpack-prerender.config.js"
 NODE_ENV=production ./build_helpers/buildSiteIndexPages.sh
--- build_helpers/startSiteDevServer.sh
+++ build_helpers/startSiteDevServer.sh
@@ -5,6 +5,6 @@ PATH=$(npm bin):$PATH
 rm -rf ./__site__
 rm -rf ./__site_prerender__
 ./build_helpers/buildAPIDocs.sh
-webpack --config site/webpack-prerender.config.js
+webpack --config "$PWD/site/webpack-prerender.config.js"
 ./build_helpers/buildSiteIndexPages.sh
-webpack-dev-server --config site/webpack-client.config.js --no-info --content-base __site__
+webpack-dev-server --config "$PWD/site/webpack-client.config.js" --no-info --content-base __site__
