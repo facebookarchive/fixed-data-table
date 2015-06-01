08b168169c389620e83f720ef5e6509a77022d40
wlis <wlis@fb.com>
2015-06-01 16:38:57 -0700

    Reducing size of row buffer

--- src/FixedDataTableRowBuffer.js
+++ src/FixedDataTableRowBuffer.js
@@ -16,8 +16,8 @@ var IntegerBufferSet = require('IntegerBufferSet');
 
 var clamp = require('clamp');
 var invariant = require('invariant');
-var MIN_BUFFER_ROWS = 5;
-var MAX_BUFFER_ROWS = 15;
+var MIN_BUFFER_ROWS = 3;
+var MAX_BUFFER_ROWS = 6;
 
 // FixedDataTableRowBuffer is a helper class that executes row buffering
 // logic for FixedDataTable. It figures out which rows should be rendered
