877f2ca084cb386646be5475d6d34d6239f0a2cd
Edwin Zhang <edwinhzhang@gmail.com>
2015-07-16 15:04:13 -0700

    renamed default cell, pass props into cell

--- src/FixedDataTableCell.react.js
+++ src/FixedDataTableCell.react.js
@@ -10,7 +10,7 @@
  * @typechecks
  */
 
-var FixedDataTableCellWrapper = require('FixedDataTableCellWrapper.react');
+var FixedDataTableCellDefault = require('FixedDataTableCellDefault.react');
 var FixedDataTableHelper = require('FixedDataTableHelper');
 var ImmutableObject = require('ImmutableObject');
 var React = require('React');
@@ -141,11 +141,11 @@ var FixedDataTableCell = React.createClass({
       })
     } else {
       content = (
-        <FixedDataTableCellWrapper
+        <FixedDataTableCellDefault
           cellHeight={props.height}
           cellWidth={props.width}>
           {props.cell}
-        </FixedDataTableCellWrapper>
+        </FixedDataTableCellDefault>
       )
     }
 
--- src/FixedDataTableRoot.js
+++ src/FixedDataTableRoot.js
@@ -12,12 +12,12 @@
 'use strict';
 
 var FixedDataTable = require('FixedDataTable.react');
-var FixedDataTableCellWrapper = require('FixedDataTableCellWrapper.react');
+var FixedDataTableCellDefault = require('FixedDataTableCellDefault.react');
 var FixedDataTableColumn = require('FixedDataTableColumn.react');
 var FixedDataTableColumnGroup = require('FixedDataTableColumnGroup.react');
 
 var FixedDataTableRoot = {
-  Cell: FixedDataTableCellWrapper,
+  Cell: FixedDataTableCellDefault,
   Column: FixedDataTableColumn,
   ColumnGroup: FixedDataTableColumnGroup,
   Table: FixedDataTable,
