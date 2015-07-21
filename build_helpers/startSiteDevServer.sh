b4828fc779eb4742e39802181a8ccadf8d95d1f7
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2015-07-21 17:48:32 +0800

    revert shell script back

--- build_helpers/startSiteDevServer.sh
+++ build_helpers/startSiteDevServer.sh
@@ -7,4 +7,4 @@ rm -rf ./__site_prerender__
 ./build_helpers/buildAPIDocs.sh
 webpack --config "$PWD/site/webpack-prerender.config.js"
 ./build_helpers/buildSiteIndexPages.sh
-webpack-dev-server --host 0.0.0.0 --config "$PWD/site/webpack-client.config.js" --no-info --content-base __site__
+webpack-dev-server --config "$PWD/site/webpack-client.config.js" --no-info --content-base __site__
