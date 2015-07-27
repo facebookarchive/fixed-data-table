fd2c071656cbe6f79cf232fa92cfa5dc6a1111c5
Edwin Zhang <edwinhzhang@gmail.com>
2015-07-27 14:01:02 -0700

    add old examples

---
+++ site/examples/old/ColumnGroupsExample.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var ROWS = 1000000;
+
+var FakeObjectDataListStore = require('./FakeObjectDataListStore');
+var FixedDataTable = require('fixed-data-table');
+var React = require('react');
+
+var Column = FixedDataTable.Column;
+var ColumnGroup = FixedDataTable.ColumnGroup;
+var PropTypes = React.PropTypes;
+var Table = FixedDataTable.Table;
+
+var ColumnGroupsExample = React.createClass({
+  propTypes: {
+    onContentDimensionsChange: PropTypes.func,
+    left: PropTypes.number,
+    top: PropTypes.number,
+  },
+
+  getInitialState() {
+    return {
+      dataList: new FakeObjectDataListStore(ROWS)
+    }
+  },
+
+  _rowGetter(index){
+    return this.state.dataList.getObjectAt(index);
+  },
+
+  render() {
+    var controlledScrolling =
+      this.props.left !== undefined || this.props.top !== undefined;
+
+    return (
+      <Table
+        rowHeight={30}
+        groupHeaderHeight={30}
+        headerHeight={30}
+        rowGetter={this._rowGetter}
+        rowsCount={this.state.dataList.getSize()}
+        width={this.props.tableWidth}
+        height={this.props.tableHeight}
+        scrollTop={this.props.top}
+        scrollLeft={this.props.left}
+        overflowX={controlledScrolling ? "hidden" : "auto"}
+        overflowY={controlledScrolling ? "hidden" : "auto"}>
+        <ColumnGroup
+          fixed={true}
+          label="Name">
+          <Column
+            fixed={true}
+            dataKey="firstName"
+            label="First Name"
+            width={150}
+          />
+          <Column
+            fixed={true}
+            label="Last Name"
+            dataKey="lastName"
+            width={150}
+          />
+        </ColumnGroup>
+        <ColumnGroup label="About">
+          <Column
+            label="Company"
+            dataKey="companyName"
+            flexGrow={1}
+            width={150}
+          />
+          <Column
+            label="Sentence"
+            dataKey="sentence"
+            flexGrow={1}
+            width={150}
+          />
+        </ColumnGroup>
+      </Table>
+    );
+  }
+});
+
+module.exports = ColumnGroupsExample;
---
+++ site/examples/old/FlexGrowExample.js
@@ -0,0 +1,93 @@
+"use strict";
+
+var ROWS = 1000000;
+
+var FakeObjectDataListStore = require('./FakeObjectDataListStore');
+var FixedDataTable = require('fixed-data-table');
+var React = require('react');
+
+var Column = FixedDataTable.Column;
+var PropTypes = React.PropTypes;
+var Table = FixedDataTable.Table;
+
+function colorizeText(/*string*/ str, key, data, index) {
+  var val, n = 0;
+  return str.split('').map((letter) => {
+    val = index * 70 + n++;
+    var color = 'hsl(' + val + ', 100%, 50%)';
+    return <span style={{color}} key={n}>{letter}</span>;
+  });
+}
+
+var FlexGrowExample = React.createClass({
+  propTypes: {
+    onContentDimensionsChange: PropTypes.func,
+    left: PropTypes.number,
+    top: PropTypes.number,
+  },
+
+  getInitialState() {
+    return {
+      dataList: new FakeObjectDataListStore(ROWS)
+    };
+  },
+
+  _rowGetter(index) {
+    return this.state.dataList.getObjectAt(index);
+  },
+
+  _onContentHeightChange(contentHeight) {
+    this.props.onContentDimensionsChange &&
+      this.props.onContentDimensionsChange(
+        contentHeight,
+        Math.max(600, this.props.tableWidth)
+      );
+  },
+
+  render() {
+    var controlledScrolling =
+      this.props.left !== undefined || this.props.top !== undefined;
+
+    return (
+      <Table
+        rowHeight={50}
+        headerHeight={50}
+        rowGetter={this._rowGetter}
+        rowsCount={this.state.dataList.getSize()}
+        width={this.props.tableWidth}
+        height={this.props.tableHeight}
+        onContentHeightChange={this._onContentHeightChange}
+        scrollTop={this.props.top}
+        scrollLeft={this.props.left}
+        overflowX={controlledScrolling ? "hidden" : "auto"}
+        overflowY={controlledScrolling ? "hidden" : "auto"}>
+        <Column
+          dataKey="firstName"
+          fixed={true}
+          label="First Name"
+          width={100}
+        />
+        <Column
+          label="Sentence! (flexGrow greediness=2)"
+          cellRenderer={colorizeText}
+          dataKey="sentence"
+          flexGrow={2}
+          width={200}
+        />
+        <Column
+          flexGrow={1}
+          label="Company (flexGrow greediness=1)"
+          width={200}
+          dataKey="companyName"
+        />
+        <Column
+          dataKey="lastName"
+          label="Last Name"
+          width={100}
+        />
+      </Table>
+    );
+  }
+});
+
+module.exports = FlexGrowExample;
