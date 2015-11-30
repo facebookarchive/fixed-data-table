cf29a107caa47c5b8f54d296bce08bdfcfb1f7b4
Evgueni Naverniouk <evgueni.naverniouk@cumul8.com>
2015-11-30 13:54:26 -0800

    Add "row" role to row elements which have onClick events

--- src/FixedDataTableRow.react.js
+++ src/FixedDataTableRow.react.js
@@ -138,7 +138,9 @@ var FixedDataTableRowImpl = React.createClass({
         onMouseDown={this.props.onMouseDown ? this._onMouseDown : null}
         onMouseEnter={this.props.onMouseEnter ? this._onMouseEnter : null}
         onMouseLeave={this.props.onMouseLeave ? this._onMouseLeave : null}
-        style={style}>
+        role="row"
+        style={style}
+        tabIndex="-1">
         <div className={cx('fixedDataTableRowLayout/body')}>
           {fixedColumns}
           {scrollableColumns}
